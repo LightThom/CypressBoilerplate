0c6ca45e98b37aa6deb4919275fae14c19f164fd
LightThom <91273024+LightThom@users.noreply.github.com>
2023-02-21 17:41:33 +0000

    Minor tweeks

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'https://www.xe.com/',
+    baseUrl: '/',
     
     setupNodeEvents(on, config) {
       // implement node event listeners here
