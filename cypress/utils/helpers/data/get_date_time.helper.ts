529016e6cc261c863f11d58f0506d1c186e3d10b
Etienne Smith <etienne.smith@xe.com>
2023-03-01 09:27:49 +0000

    More changes

--- cypress/utils/helpers/data/get_date_time.helper.ts
+++ cypress/utils/helpers/data/get_date_time.helper.ts
@@ -1,13 +1,13 @@
 export class TestData_GetDateTime {
     constructor() {}
-<<<<<<< HEAD
 
+    //DO NOTE USE: below is an example only
     async getDateTimeNow() {
         function padTo2Digits(num: number) {
-            return num.toString().padStart(2, '0');
+            //return num.toString().padStart(2, '0');
         }
         function padTo3Digits(num: number) {
-            return num.toString().padStart(3, '0');
+            //return num.toString().padStart(3, '0');
         }
 
         function formatDate(date: Date) {
@@ -32,33 +32,3 @@ export class TestData_GetDateTime {
         };
     }
 }
-=======
-  
-    async getDateTimeNow() {
-      function padTo2Digits(num: number) {
-        return num.toString().padStart(2, '0');
-      }
-      function padTo3Digits(num: number) {
-        return num.toString().padStart(3, '0');
-      }
-  
-      function formatDate(date: Date) {
-        return (
-          [date.getFullYear(), padTo2Digits(date.getMonth() + 1), padTo2Digits(date.getDate())].join('-') +
-          ' ' +
-          [
-            padTo2Digits(date.getHours()),
-            padTo2Digits(date.getMinutes()),
-            padTo2Digits(date.getSeconds()),
-            padTo3Digits(date.getMilliseconds()),
-          ].join(':')
-        );
-      }
-      console.log(formatDate(new Date()));
-      return {
-        dateTimeNow: formatDate(new Date()),
-      };
-    }
-  }
-  
->>>>>>> fc427a016600df33a6ccb83a514b38c4bf125f5d
