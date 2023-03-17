2aca1768ef6a2e94a8436aa00778d4358dedb78c
Josh <josh.olan@yahoo.com>
2023-03-17 15:30:38 +0000

    added files

--- cypress/utils/constants/device_constants.ts
+++
@@ -1,8 +0,0 @@
-export const DEVICE = {
-    // Device Details
-    device: {
-        deviceId: `integration_device_id`,
-        deviceToken: 'integration_device_token',
-        devicePlatform: 'Android'
-    }
-};
